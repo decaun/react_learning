79e4efbd17c2283c35841690f1ae10821f28dd34
decaun <denizcanuner@gmail.com>
2019-08-07 15:52:10 -0500

    connected reducer

---
+++ reactReduxNewBoston/tutorial/src/components/Appl.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Appl = () => (
+    <div>
+        <h2>User name List:</h2>
+            <hr/>
+        <h2>User Details:</h2>
+        
+    </div>
+);
+
+export default Appl;
